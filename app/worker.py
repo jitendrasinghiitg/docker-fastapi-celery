46a5d1c110ff2e0346ff44921eb217502e052731
chickooooo <kshitijrajale@gmail.com>
2024-01-09 21:05:09 +0530

    updated dependencies & refactored code.

--- app/worker.py
+++ app/worker.py
@@ -1,8 +1,15 @@
 import os
 from celery import Celery
 
+
+# celery broker and backend urls
 CELERY_BROKER_URL = os.getenv("REDISSERVER", "redis://redis_server:6379")
 CELERY_RESULT_BACKEND = os.getenv("REDISSERVER", "redis://redis_server:6379")
 
-celery = Celery("celery", backend=CELERY_BROKER_URL, broker=CELERY_RESULT_BACKEND)
 
+# create celery application
+celery_app = Celery(
+    "celery",
+    backend=CELERY_BROKER_URL,
+    broker=CELERY_RESULT_BACKEND,
+)
